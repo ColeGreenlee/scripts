59e44f6bb628236920d508d2568c39155feaeef0
Cole Greenlee <ColeTGreenlee@gmail.com>
2019-10-20 01:24:12 -0500

    Updated LRT Headers

--- east_bank_trains.sh
+++ east_bank_trains.sh
@@ -2,6 +2,6 @@ while true; do
   i=$(curl -s https://svc.metrotransit.org/NexTrip/902/2/EABK\?format\=json  | jq -r '.[] | "\(.DepartureText)"')
   j=$(curl -s https://svc.metrotransit.org/NexTrip/902/3/EABK\?format\=json  | jq -r '.[] | "\(.DepartureText)"')
   clear;
-  echo "EASTBOUND\n=========\n$i\n\nWESTBOUND\n=========\n$j" | lolcat -a -d 1 -S 10
-  sleep 10;
+  echo "LRT EASTBOUND\n=============\n$i\n\nLRT WESTBOUND\n=============\n$j" | lolcat -a -d 1 -S 15
+  sleep 30;
 done
